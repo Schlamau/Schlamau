4359a4b2540b03d67d927b993ed4562ea9b26eca
Schlamau <74138251+Schlamau@users.noreply.github.com>
2020-12-31 14:46:32 +0100

    Add files via upload

---
+++ javascript/footer_level1.js
@@ -0,0 +1,10 @@
+var lnk = " ";
+
+lnk +=  '<ul>'
+lnk +=  '<li><a href="../footer/ueber.html">&Uuml;ber</a></li>'
+lnk +=  '<li><a href="../footer/kontakt.html">Kontakt</a></li>'
+lnk +=	'<li><a href="../footer/danksagung.html">Danksagung</a></li>'
+lnk +=	'<li><a href="../footer/impresum.html">Impresum</a></li>'
+lnk +=	'</ul>'
+
+document.write(lnk);
---
+++ javascript/nav-bar_level1.js
@@ -0,0 +1,30 @@
+var lnk = " ";
+
+lnk += '<nav>'
+lnk += '<ul>'
+lnk += '<li>'
+lnk += '<a href="../../index.html" id="active"> Home </a>'
+lnk += '</li>'
+lnk += '<li class="dropdown">'
+
+
+lnk += '<a href="#touristik" class="dropbtn"> Touristik </a>'
+lnk += '<div class="dropdown-content">'
+lnk += '<a href="../../sites/touristik/essen.html"> Essen </a>'
+lnk += '<a href="../../sites/touristik/freizeit.html"> Freizeit </a>'
+lnk += '<a href="../../sites/touristik/sehenswuerdigkeiten.html"> Sehensw&uuml;rdigkeiten </a>'
+lnk += '</div>'
+lnk += '</li>'
+lnk += '<li class="dropdown">'
+
+
+lnk += '<a href="#firmen" class="dropbtn"> Firmen </a>'
+lnk += '<div class="dropdown-content">'
+lnk += '<a href="../../sites/firmen/birgit_kaiser_baer.html"> Birgit Kaiser B&auml;r</a>'
+lnk += '<a href="../../sites/firmen/heiko_neumann_reifen.html"> Heiko Neumann Reifen </a>'
+lnk += '</div>'
+lnk += '</li>'
+lnk += '</ul>'
+lnk += '</nav>'
+
+document.write(lnk);
